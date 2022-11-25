e14a0c32e25912358113503656038fdda8e8d285
Shishir <shishir.kan123456789@gmail.com>
2022-11-25 18:37:57 +0545

    added urls and main files changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
 
 void main() {
   runApp(
@@ -16,8 +18,61 @@ void main() {
               KhaltiLocalizations.delegate,
             ],
         initialRoute: '/login',
-        routes: const {
-          },
+        routes: {
+          '/bottomNavBar': (context) => const BottomNavBar(),
+
+          // Event Bookingdis
+          '/chooseVenue': (context) => const ChooseVenueScreen(),
+          '/chooseEvent': (context) => const ChooseEventScreen(),
+          '/chooseTheme':(context) => const ChooseThemeScreen(),
+          '/chooseDrinks':(context) => const ChooseDrinksScreen(),
+          '/chooseDrinksCategory':(context) => const ChooseDrinksCategoryScreen(),
+          '/chooseCakes':(context) => const ChooseCakesScreen(),
+          '/chooseCreateEventScreen':(context)=> const ChooseCreateEventScreen(),
+          '/chooseDecorationScreen':(context)=> const ChooseDecorationsScreen(),
+          '/detailsScreen':(context) => const DetailsScreen(),
+
+          // Edit Selections
+          '/editVenue' :(context) => const EditVenueScreen(),
+          '/editEvent' :(context) => const EditEventScreen(),
+          '/editTheme':(context) => const EditThemeScreen(),
+          '/editCakes':(context) => const EditCakesScreen(),
+          '/editDecoration':(context) => const EditDecorationsScreen(),
+          '/editDrinks':(context) => const EditDrinksScreen(),
+          '/editDrinksCategory':(context) => const EditDrinksCategoryScreen(),
+          '/editDetailsScreen':(context) => const EditDetailsScreen(),
+
+          // Khalti Payment
+          '/khaltiPaymentScreen':(context) =>const KhaltiPaymentScreen(),
+          '/khaltiPaymentDraftScreen':(context) =>const KhaltiPaymentDraftScreen(),
+          
+          // Auth User
+          '/login':(context) => const LoginScreen(),
+          '/register':(context) => const RegisterScreen(),
+          '/OTPVerification':(context) => const OTPVerification(),
+          '/forgotPassword':(context) => const ForgotPasswordScreen(),
+          '/resetPassword':(context) => const ResetPasswordScreen(),
+          '/changePassword':(context) => const ChangePasswordScreen(),
+
+          // Chat with Staff
+          // '/chat':(context) => const EventXChatPage(),
+          '/chatStaff':(context) => const StaffChatPage(),
+
+          // Draft Events
+          '/detailsScreenDraft':(context) => const DetailsScreenDraft(),
+          '/eventTrack':(context) => const TrackEvent(),
+          '/savedDraft':(context) => const SavedDraftScreen(),
+
+          // for trying
+          '/pastEventsScreen':(context) => const PastBookedEventsScreen(),
+
+          // for Profile
+          '/editProfile':(context) => const EditProfileScreen(),
+          '/termsAndCondition':(context) => TermsConditionsPage(),
+          '/contactUs':(context) => ContactUsPage(),
+          '/aboutUs':(context) => AboutUsPage(),
+          '/notificationScreen':(context) => const NotificationScreen(),
+        },
       );}
     ),
   );
