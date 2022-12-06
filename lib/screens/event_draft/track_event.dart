bcbcd0d3dbb1b5381d7d4e794d25faae012b2dc7
Pasang <pasangyangchen09@gmail.com>
2022-12-06 18:28:31 +0545

    added track event after event booking

---
+++ lib/screens/event_draft/track_event.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+
+class TrackEvent extends StatefulWidget {
+  const TrackEvent({Key? key}) : super(key: key);
+
+
+  @override
+  State<TrackEvent> createState() => _TrackEventState();
+}
+
+class _TrackEventState extends State<TrackEvent> {
+
+  var eventBooking;
+  final storage = GetStorage();
+
+  @override
+  Widget build(BuildContext context) {
+    // eventBooking = ModalRoute.of(context)!.settings.arguments as Map;
+    // debugPrint("Details Screen: $eventBooking");
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 250, 250, 255),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            width: 50,
+                            height: 25,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              color: Colors.blue,
+                              // border:
+                            ),
+                            alignment: Alignment.center,
+                            child: const Text(
+                              "Back",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 80,
+                      ),
+                      const Text(
+                        "Track Event",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromRGBO(118, 125, 152, 1),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  
+                  
+                  Container(
+                    height: 60,
+                    padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    alignment: Alignment.centerLeft,
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Decoration",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromARGB(210, 118, 125, 152),
+                          ),
+                        ),
+                        Text(
+                          "InProgress",
+                          style: TextStyle(
+                            color: Color.fromARGB(228, 209, 59, 0),
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Container(
+                    height: 60,
+                    padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    alignment: Alignment.centerLeft,
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Drinks",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromARGB(210, 118, 125, 152),
+                          ),
+                        ),
+                        Text(
+                          "In Progress",
+                          style: TextStyle(
+                            color: Color.fromARGB(228, 209, 59, 0),
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Container(
+                    height: 60,
+                    padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    alignment: Alignment.centerLeft,
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Cakes",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromARGB(210, 118, 125, 152),
+                          ),
+                        ),
+                        Text(
+                          "InProgress",
+                          style: TextStyle(
+                            color: Color.fromARGB(228, 209, 59, 0),
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  
+                  const SizedBox(height: 15),
+                  
+                  Center(
+                    child: SizedBox(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: ElevatedButton(
+                        key: const ValueKey('btnLogin'),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              // side: const BorderSide(color: Colors.red),
+                            ),
+                          ),
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color.fromRGBO(0, 131, 29, 1)),
+                        ),
+                        child: const Text(
+                          "Continue",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
